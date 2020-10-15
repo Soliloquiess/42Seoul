bc49f5f558bf98ace027dc3d464b23bd59d25a29
Soliloquiess <cyh1219@naver.com>
2020-10-15 21:12:26 +0900

    replace

--- c00/ex01/ft_print_alphabet.c
+++ c00/ex01/ft_print_alphabet.c
@@ -1,30 +1,13 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_print_alphabet.c                                :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: ecaceres <marvin@42.fr>                    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/07/31 11:09:32 by ecaceres          #+#    #+#             */
-/*   Updated: 2019/07/31 11:09:45 by ecaceres         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include <unistd.h>
-
-void	ft_putchar(char c)
-{
-	write(1, &c, 1);
-}
 
 void	ft_print_alphabet(void)
 {
-	char letter;
+	char alphabet;
 
-	letter = 'a';
-	while (letter <= 'z')
+	alphabet = 'a';
+	while (alphabet <= 'z')
 	{
-		ft_putchar(letter);
-		letter++;
+		write(1,&alphabet,1);
+		alphabet++;
 	}
 }
